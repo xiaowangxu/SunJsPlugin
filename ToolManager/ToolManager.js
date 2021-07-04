6fd27f4c06f246301def40b5b4301fa91442d098
王徐 <2208740924@qq.com>
2021-07-04 11:19:16 +0800

    ToolManager refactor

--- ToolManager/ToolManager.js
+++ ToolManager/ToolManager.js
@@ -1,153 +1,9 @@
-export class ToolManager {
-	constructor(log = () => { }, fin = () => { }) {
-		this.micro_tools = new Array()
-		this.current_tool = null
-		this.global_data = {}
-		this.last_param = {}
-		this.graph = null
-		this.log_func = log
-		this.fin_func = fin
-	}
+///////////////////////////////////
+//        SUN ToolManager
+///////////////////////////////////
+// import { HTML } from '../Utils.js';
 
-	add_MicroTool(microtool) {
-		if (!(microtool instanceof MicroTool)) {
-			throw new Error("microtoll is not an instance of MicroTool Class")
-		}
-
-		for (let i = 0; i < this.micro_tools.length; i++) {
-			if (microtool.name === this.micro_tools[i].name) {
-				throw new Error("microtool name already existed")
-			}
-		}
-
-		this.micro_tools.push(microtool)
-		microtool.uid = this.micro_tools.length
-	}
-
-	get_MicroTool(toolname) {
-		for (let i = 0; i < this.micro_tools.length; i++) {
-			if (toolname === this.micro_tools[i].name) {
-				return this.micro_tools[i]
-			}
-		}
-		return null
-	}
-
-	turnTo(toolname, param) {
-		this.last_param = param
-		let tool = this.get_MicroTool(toolname)
-		if (tool === null) {
-			throw new Error("MicroTool not found")
-		}
-		this.current_tool = tool
-		this.log_func('log', this.current_tool.nickname + " " + this.current_tool.description)
-		if (this.current_tool.autorun !== undefined) {
-			this.current_tool.reset_Data()
-			return this.current_tool.autorun(this, this.global_data, this.last_param)
-		}
-		return null
-	}
-
-	send_Param(param) {
-		this.last_param = param
-	}
-
-	emit(signal, param = {}) {
-		signal = this.current_tool.name + "_" + signal
-		if (this.graph.signals[signal] !== undefined) {
-			this.graph.signals[signal](this.global_data, param)
-		}
-	}
-
-	call(func, param = {}) {
-		if (this.current_tool !== null && this.current_tool[func] !== undefined) {
-			let result = (this.current_tool[func])(this, this.global_data, param, this.last_param)
-			if (result !== null) {
-				if (result.flag === "ERROR") {
-					this.current_tool = null
-					this.global_data = {}
-					this.signals = {}
-					this.graph = null
-					this.log_func('error', result.param.errorMsg)
-					this.fin_func()
-					return
-				}
-				else if (result.flag === "END") {
-					this.current_tool = null
-					this.global_data = {}
-					this.signals = {}
-					this.graph = null
-					this.fin_func()
-					// comment
-					// console.log("Graph Ended")
-					return
-				}
-				let transform = this.graph.advance(result.flag)
-				if (transform !== null && this.graph.transforms[transform] !== undefined) {
-					// comment
-					// console.log("-> Transform: " + transform)
-					result.param = this.graph.transforms[transform](this.global_data, result.param)
-				}
-				this.run(this.graph, result.param, this.global_data)
-				return
-			}
-		}
-	}
-
-	run(graph, param, global = {}) {
-		if (this.graph !== null && this.graph !== graph) {
-			throw new Error("Has FlowGraph in progress")
-		}
-		this.graph = graph
-		this.global_data = global
-		if (this.graph.current_node === null) {
-			this.current_tool = null
-			this.global_data = {}
-			this.signals = {}
-			this.graph = null
-			this.fin_func()
-			return
-		}
-		let result = this.turnTo(this.graph.current_node.name, param)
-		while (result !== null) {
-			if (result.flag === "ERROR") {
-				this.current_tool = null
-				this.global_data = {}
-				this.signals = {}
-				this.graph = null
-				this.log_func('error', result.param.errorMsg)
-				this.fin_func()
-				return
-			}
-			else if (result.flag === "END") {
-				this.current_tool = null
-				this.global_data = {}
-				this.signals = {}
-				this.graph = null
-				this.fin_func()
-				return
-			}
-			let transform = this.graph.advance(result.flag)
-			if (this.graph.current_node === null) {
-				this.current_tool = null
-				this.global_data = {}
-				this.signals = {}
-				this.graph = null
-				this.fin_func()
-				// comment
-				// console.log("Graph Ended 1")
-				return
-			}
-			if (transform !== null && this.graph.transforms[transform] !== undefined) {
-				// comment
-				// console.log("-> Transform: " + transform)
-				result.param = this.graph.transforms[transform](this.global_data, result.param)
-			}
-			result = this.turnTo(this.graph.current_node.name, result.param)
-		}
-	}
-}
-
+// class MicroTool
 export class MicroTool {
 	constructor(name, nickname = '', description = '', reset = () => { return {} }) {
 		this.name = name
@@ -156,7 +12,7 @@ export class MicroTool {
 		this.reset = reset
 		this.data = this.reset()
 		this.nickname = nickname === '' ? this.name : nickname
-		this.description = description === '' ? this.name : description
+		this.description = description
 	}
 
 	reset_Data() {
@@ -164,6 +20,7 @@ export class MicroTool {
 	}
 }
 
+// class FlowGraph
 export class FlowGraph {
 	constructor(pathstr, startname, transforms = {}, signals = {}) {
 		let path = pathstr.split('|')
@@ -174,7 +31,10 @@ export class FlowGraph {
 
 		for (let i = 0; i < path.length; i++) {
 			let pathitem = path[i].split('-')
-			if (pathitem.length === 2) {
+			if (pathitem.length === 1) {
+				let nodeFromIndex = this.create_Node(pathitem[0])
+			}
+			else if (pathitem.length === 2) {
 				let nodeFromIndex = this.create_Node(pathitem[0])
 				let nodeToIndex = this.create_Node(pathitem[1])
 				this.nodes[nodeFromIndex].add_Path('ALL', nodeToIndex)
@@ -192,6 +52,9 @@ export class FlowGraph {
 		}
 
 		let start_index = this.get_NodeIndex(startname)
+		if (start_index === null) {
+			throw new Error('Can not find startnode with name of \'' + startname + '\'')
+		}
 		this.current_node = this.nodes[start_index]
 	}
 
@@ -214,15 +77,16 @@ export class FlowGraph {
 	}
 
 	create_Node(name) {
-		let node = this.get_NodeIndex(name)
+		let n = name.split(':')[0]
+		let node = this.get_NodeIndex(n)
 		if (node !== null) return node
 		this.nodes.push(new FlowGraphNode(name))
 		return this.nodes.length - 1
 	}
 
 	advance(flag) {
-		// console.log("-> " + flag)
 		let path = this.current_node.get_Path(flag)
+		// //console.log(path)
 		if (path !== null) {
 			this.current_node = this.nodes[path.next]
 			return path.transform
@@ -236,7 +100,12 @@ export class FlowGraph {
 
 class FlowGraphNode {
 	constructor(name) {
-		this.name = name
+		let names = name.split(':')
+		this.raw_name = name
+		this.name = names[0]
+		this.toolname = this.name.split('#')[0]
+		this.params = names[1] !== undefined ? names[1].split(',') : []
+		// the map of the flag and the next node
 		this.path = new Array()
 	}
 
@@ -247,7 +116,7 @@ class FlowGraphNode {
 	get_Path(flag) {
 		for (let i = 0; i < this.path.length; i++) {
 			let pathitem = this.path[i]
-			if (pathitem.flag === flag) {
+			if (pathitem.flag === 'ALL' || pathitem.flag === flag) {
 				return pathitem
 			}
 		}
@@ -258,17 +127,155 @@ class FlowGraphNode {
 		if (this.path.length <= 0) return true
 		return false
 	}
+}
 
-	get_Next(flag) {
-		for (let i = 0; i < this.path.length; i++) {
-			let pathitem = this.path[i]
-			if (pathitem.flag === 'all') {
-				return pathitem.next
+// class ToolManager
+export class ToolManager {
+	constructor(log = (type, info) => { }, fin = () => { }) {
+		this.micro_tools = {}
+		this.current_tool = null
+		this.global_data = {}
+		this.last_param = {}
+		this.graph = null
+		this.log_func = log
+		this.fin_func = fin
+		this.graph_stack = []
+		this.global_data_stack = []
+		this.allow_input = true
+	}
+
+	add_MicroTool(microtool) {
+		if (!(microtool instanceof MicroTool)) {
+			throw new Error("microtoll is not an instance of MicroTool Class")
+		}
+
+		if (this.micro_tools[microtool.name] !== undefined)
+			throw new Error("microtool name already existed")
+
+		this.micro_tools[microtool.name] = microtool
+		// microtool.uid = this.micro_tools.length
+	}
+
+	equip_Tools(obj) {
+		for (let key in obj) {
+			this.add_MicroTool(obj[key])
+		}
+	}
+
+	get_MicroTool(toolname) {
+		if (this.micro_tools[toolname] === undefined) return null
+		else return this.micro_tools[toolname]
+	}
+
+	new_MicroTool(toolname) {
+		let tool = this.get_MicroTool(toolname)
+		this.current_tool = tool
+		// tool.reset_Data()
+	}
+
+	get_ToolStyledHTML() {
+		if (this.current_tool !== null) {
+			let str = "<span style=\"padding: 4px 8px ; background-color:rgb(240, 70, 60); border-radius: var(--ObjectRadius) 0px 0px var(--ObjectRadius); color:white;font-weight:bold;\">微工具</span><span style=\"padding: 4px 8px ; background-color: rgb(50,50,50); border-radius: 0px var(--ObjectRadius) var(--ObjectRadius) 0px; color: white;\">" + this.current_tool.nickname + "</span>"
+			return str
+		}
+		return ''
+	}
+
+	// 
+	log(type, msg) {
+		// this.log_func(type, 'ToolManager', HTML.create_TabOr([this.get_ToolStyledHTML(), msg]))
+	}
+
+	emit(signal, param = {}) {
+		signal = this.graph.current_node.name + "_" + signal
+		// console.log(signal)
+		if (this.graph.signals[signal] !== undefined) {
+			return this.graph.signals[signal](this.global_data, param, this.graph.current_node.params)
+		}
+	}
+
+	call(func, param = {}) {
+		if (this.allow_input && this.current_tool !== null && this.current_tool[func] !== undefined) {
+			this.step(func, param)
+		}
+	}
+
+	// running
+	start_SubGraph(graph, param, global) {
+		this.global_data_stack.push(this.global_data)
+		this.graph_stack.push(this.graph)
+		this.global_data = global
+		this.graph = graph
+		this.last_param = param
+	}
+
+	end_SubGrpah() {
+		if (this.graph_stack.length === 0 || this.graph_stack[this.graph_stack.length - 1] === null) {
+			this.end()
+			return false;
+		}
+		// console.error("end Subgraph", result)
+		this.global_data = this.global_data_stack.pop()
+		this.graph = this.graph_stack.pop()
+		return true;
+	}
+
+	step(func = 'autorun', callevent = undefined) {
+		let result = null
+		while (this.graph.current_node !== null) {
+			let { toolname, name, params } = this.graph.current_node
+			if (toolname === 'SubGraph') {
+				this.start_SubGraph(this.last_param.subgraph, this.last_param.param || {}, this.last_param.global || {})
 			}
-			if (pathitem.flag === flag) {
-				return pathitem.next
+			else {
+				this.new_MicroTool(toolname)
+				if (func === 'autorun') this.current_tool.reset_Data()
+				if (this.current_tool[func] !== undefined) {
+					result = this.current_tool[func](this, this.global_data, this.last_param, callevent, params)
+					func = 'autorun'
+					if (result !== null) {
+						let transform = this.graph.advance(result.flag)
+						if (this.graph.current_node === null) {
+							if (!this.end_SubGrpah()) {
+								return
+							}
+							transform = this.graph.advance(result.flag)
+						}
+						let transformfunc = this.graph.transforms[transform]
+						if (transform !== "" && transformfunc !== undefined) {
+							result.param = transformfunc(this.global_data, result.param, params)
+						}
+						this.last_param = result.param
+
+					}
+					else {
+						return
+					}
+				}
+				else
+					throw new Error(`function name ${func} not found in microtool ${this.current_tool.name}`)
 			}
 		}
-		return -1
+		this.end_SubGrpah()
+	}
+
+	run(graph, param = {}, global = {}) {
+		if (this.is_Running()) return new Error('ToolManager is still running')
+		this.start_SubGraph(graph, param, global)
+		this.step()
+	}
+
+	end() {
+		this.current_tool = null
+		this.global_data = {}
+		this.graph = null
+		this.last_param = {}
+		this.graph_stack = []
+		this.global_data_stack = []
+		this.fin_func()
+	}
+
+	is_Running() {
+		return (this.graph !== null)
 	}
 }
